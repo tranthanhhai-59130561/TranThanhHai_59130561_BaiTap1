928fc9511c5a0e01b8632648a686585c6c6c864d
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-12 21:19:25 +0700

    BaiTap1

---
+++ BaiTap1/src/baitap1/IQLDS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap1;
+import java.util.ArrayList;
+/**
+ *
+ * @author Admin
+ */
+public interface IQLDS {
+    public int them(CaNhan caNhan);
+    public int xoa(String ten);
+    public void inDS(ArrayList<CaNhan> ds);
+}
