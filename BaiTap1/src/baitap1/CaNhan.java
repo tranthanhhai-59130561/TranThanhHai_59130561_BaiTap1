65e5bb64643d48fa401dee7ac0e290b98466b14c
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-12 20:45:40 +0700

    BaiTap1

--- BaiTap1/src/baitap1/CaNhan.java
+++ BaiTap1/src/baitap1/CaNhan.java
@@ -10,5 +10,58 @@ package baitap1;
  * @author Admin
  */
 public class CaNhan {
+    public String hoTen;
+    public int tuoi;
+    public String diaChi;
+    public String sdt;
     
+    public CaNhan(String hoTen, int tuoi, String diaChi, String sdt)
+    {
+        this.hoTen = hoTen;
+        this.tuoi = tuoi;
+        this.diaChi = diaChi;
+        this.sdt = sdt;
+    }
+    
+    public String getHoten()
+    {
+        return hoTen;
+    }
+    
+    public void setHoten(String hoTen)
+    {
+        this.hoTen = hoTen;
+    }
+    
+    public int getTuoi()
+    {
+        return tuoi;
+    }
+    
+    public void getTuoi(int tuoi)
+    {
+        this.tuoi = tuoi;
+    }
+    
+    public String getDiachi()
+    {
+        return diaChi;
+    }
+    
+    public void setDiachi(String diaChi)
+    {
+        this.diaChi = diaChi;
+    }
+    
+    public String getSdt()
+    {
+        return sdt;
+    }
+    
+    public void setSdt(String sdt)
+    {
+        this.sdt = sdt;
+    }
+    
+    abstract String HienThiTT();
 }
