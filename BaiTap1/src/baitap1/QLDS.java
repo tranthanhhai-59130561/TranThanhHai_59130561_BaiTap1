5a1b5cd66b6f78dc976a0f67a5cba776d7a096c5
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-12 21:39:40 +0700

    BaiTap1

--- BaiTap1/src/baitap1/QLDS.java
+++ BaiTap1/src/baitap1/QLDS.java
@@ -4,11 +4,50 @@
  * and open the template in the editor.
  */
 package baitap1;
+import java.util.ArrayList;
 
 /**
  *
  * @author Admin
  */
-public class QLDS {
-    
+public class QLDS implements IQLDS {
+    public ArrayList<CaNhan> ds = new ArrayList<>();
+    public ArrayList<CaNhan> getDs() 
+    {
+        return ds;
+    }
+
+    public void setDs(ArrayList<CaNhan> ds) 
+    {
+        this.ds = ds;
+    }
+
+    public int them(CaNhan cn) {       
+        ds.add(cn);
+        return 1;
+    }
+
+    public int xoa(String ten) 
+    {     
+            int j = -1;
+            for (int i = 0; i < ds.size(); i++) 
+            {
+                if (ten.equals(ds.get(i).getHoten())) 
+                {
+                    j = i;
+                    break;
+                }
+            }
+            if (j > -1) 
+            {
+                ds.remove(j);
+                return 1;
+            } 
+            else
+            return 0;               
+    }
+   
+        public void inDS() {
+      ds.forEach(cn -> System.out.println(cn.HienThiTT()));
+    }    
 }
